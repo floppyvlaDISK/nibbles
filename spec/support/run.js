30cf6c28f5eb14b8fb375c41cbc2cd9d417d6bba
vlad <tarasenkokansasvlad@gmail.com>
2019-12-01 22:17:35 +0200

    Add jsdom

--- spec/support/run.js
+++ spec/support/run.js
@@ -1,4 +1,10 @@
+import jsdom from 'jsdom';
 import Jasmine from 'jasmine';
+
+const { window } = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+
+global.window = window;
+global.document = window.document;
 
 const jasmine = new Jasmine();
 jasmine.loadConfigFile('spec/support/jasmine.json');
