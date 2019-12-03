991c38543685aafe6dc0896960ea2cd98292f94d
vlad <tarasenkokansasvlad@gmail.com>
2019-12-03 23:38:24 +0200

    Test isArrowKey()

---
+++ spec/isArrowKey.spec.js
@@ -0,0 +1,26 @@
+import isArrowKey, { ARRROW_UP, ARROW_RIGHT, ARROW_DOWN, ARROW_LEFT } from '../src/utils/isArrowKey';
+
+describe('isArrowKey', () => {
+  it('arrow up key', () => {
+    expect(isArrowKey(ARRROW_UP)).toBe(true);
+  });
+
+  it('arrow right key', () => {
+    expect(isArrowKey(ARROW_RIGHT)).toBe(true);
+  });
+
+  it('arrow down key', () => {
+    expect(isArrowKey(ARROW_DOWN)).toBe(true);
+  });
+
+  it('arrow left key', () => {
+    expect(isArrowKey(ARROW_LEFT)).toBe(true);
+  });
+
+  it('other keys', () => {
+    expect(isArrowKey(13)).toBe(false);
+    expect(isArrowKey(36)).toBe(false);
+    expect(isArrowKey(41)).toBe(false);
+    expect(isArrowKey(132)).toBe(false);
+  });
+})
