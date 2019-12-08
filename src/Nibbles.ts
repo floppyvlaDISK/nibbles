2aa7c07c1a01d4579973c530f40a3a787b1c1830
vlad <tarasenkokansasvlad@gmail.com>
2019-12-08 11:40:04 +0200

    Convert Nibbles to ts

--- src/Nibbles.ts
+++ src/Nibbles.ts
@@ -1,37 +1,49 @@
 import Snake from './Snake';
+import Renderer from './Renderer';
+import BoardObject from './BoardObject';
 
 export default class Nibbles {
-  constructor(renderer, snake, target, walls) {
+  private _renderer: Renderer;
+  private _snake: Snake;
+  private _target: BoardObject;
+  private _walls: Array<BoardObject>;
+  private _snakeDirectionsQueue: Array<string>;
+
+  constructor(
+    renderer: Renderer,
+    snake: Snake,
+    target: BoardObject,
+    walls: Array<BoardObject>,
+  ) {
     this._renderer = renderer;
     this._snake = snake;
     this._target = target;
     this._walls = walls;
-    this._intervalId = null;
     this._snakeDirectionsQueue = [];
 
     this._performUpdate = this._performUpdate.bind(this);
   }
 
-  render() {
+  public render() {
     this._renderer.renderBoard();
     this._getBoardObjects().forEach(
       obj => this._renderer.renderBoardObject(obj)
     );
   }
 
-  start() {
+  public start() {
     this._performUpdate();
-    this._intervalId = setInterval(this._performUpdate, 750);
+    setInterval(this._performUpdate, 750);
   }
 
-  setSnakeDirectionFromKeyCode(value) {
+  public setSnakeDirectionFromKeyCode(value: number) {
     const latest = [...this._snakeDirectionsQueue].pop();
     if (latest !== Snake.directionFromKeyCode(value)) {
       this._snakeDirectionsQueue.push(Snake.directionFromKeyCode(value));
     }
   }
 
-  _getBoardObjects() {
+  private _getBoardObjects() {
     return [
       this._target,
       ...this._walls,
@@ -39,17 +51,17 @@ export default class Nibbles {
     ];
   }
 
-  _performUpdate() {
+  private _performUpdate() {
     this._trySetSnakeDirectionFromQueue();
     this._updateBoardObjectsPositions();
     this.render();
   }
 
-  _updateBoardObjectsPositions() {
+  private _updateBoardObjectsPositions() {
     this._snake.move();
   }
 
-  _trySetSnakeDirectionFromQueue() {
+  private _trySetSnakeDirectionFromQueue() {
     const nextDirection = this._snakeDirectionsQueue.shift();
     if (nextDirection) {
       this._snake.direction = nextDirection;
