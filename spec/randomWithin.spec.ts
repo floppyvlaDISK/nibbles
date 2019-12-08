f3071303bfce55284902f2367b31f937897c6134
vlad <tarasenkokansasvlad@gmail.com>
2019-12-08 19:53:42 +0200

    Implement randomWithin()

---
+++ spec/randomWithin.spec.ts
@@ -0,0 +1,16 @@
+import randomWithin from '../src/utils/randomWithin';
+
+describe('randomWithin', () => {
+  [
+    [0, 1],
+    [40, 760],
+    [-100, -50],
+  ].forEach(([min, max]) => {
+    it(`produces value within range: (${min}, ${max})`, () => {
+      const result = randomWithin(min, max);
+      expect(result).toBeLessThanOrEqual(max);
+      expect(result).toBeGreaterThanOrEqual(min);
+      expect(result - Math.round(result)).toBe(0);
+    });
+  });
+})
