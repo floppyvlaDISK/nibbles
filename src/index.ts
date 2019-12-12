13cbf3ce60c92ea9e39ceaa254324d6026a14066
vlad <tarasenkokansasvlad@gmail.com>
2019-12-13 00:04:39 +0200

    Render ScoreDashboard test data

--- src/index.ts
+++ src/index.ts
@@ -25,5 +25,6 @@ const aNibbles = new Nibbles(aRenderer, aSnake, target, walls, aPubSub);
 const aGameController = new GameController(aNibbles);
 const aScoreDashboard = new ScoreDashboard(container);
 
+aScoreDashboard.render(10);
 // aPubSub.subscribe('SnakeScoreChanged', aScoreDashboard);
 aGameController.exec();
